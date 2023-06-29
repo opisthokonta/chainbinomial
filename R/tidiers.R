4aedd4296a5e78426b297bc23a23e2a33d0cccb8
Jonas Christoffer Lindstrøm <JonasChristoffer.Lindstrom@fhi.no>
2023-06-29 13:27:09 +0200

    new glance method for cbmod

--- R/tidiers.R
+++ R/tidiers.R
@@ -7,7 +7,7 @@ generics::tidy
 
 #' Tidy a cbmod object
 #'
-#' @param x A `lm` object.
+#' @param x A `cbmod` object.
 #' @param conf.int Logical indicating whether or not to include a confidence interval in the tidied output. Defaults to FALSE.
 #' @param conf.level The confidence level to use for the confidence interval if conf.int = TRUE. Defaults to 0.95, which corresponds to a
 #'   95 percent confidence interval.
@@ -21,7 +21,7 @@ tidy.cbmod <- function(x, conf.int=FALSE, conf.level = 0.95, ...){
 
   res <- tibble::tibble(term = names(x$parameters),
                         estimate = x$parameters,
-                        str.error = x$se)
+                        std.error = x$se)
 
   if (conf.int) {
     ci <- confint(x, level = conf.level)
@@ -34,4 +34,24 @@ tidy.cbmod <- function(x, conf.int=FALSE, conf.level = 0.95, ...){
 }
 
 
+
+#' @importFrom generics glance
+#' @export
+generics::glance
+
+#' Glance at a cbmod object
+#'
+#' @param x A `cbmod` object.
+#' @param ... Unused.
+#'
+#' @export
+glance.cbmod <- function(x, ...){
+
+
+  res <- tibble::tibble(logLik = x$loglikelihood,
+                        npar = x$npar)
+
+  return(res)
+
+}
 
